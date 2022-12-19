8a7c585fd0e0f4b1fd276436b351b46635a0d1ae
MehdiDissem <mehdissem@gmail.com>
2022-12-19 08:26:54 +0100

    contact us

--- client/pages/Contact.tsx
+++ client/pages/Contact.tsx
@@ -1,42 +1,43 @@
 
+
 export default function Contact(){
   return <div>
-    <main className="ddd">
-        <h1 className="ddd">
-          Welcome to <a href="/Home">LABASNI</a> SPORTS SHOP
-        </h1>
+   <section id="contact">
+			<div className="section-content">
+				<h1 className="section-header">Get in <span className="content-header wow fadeIn " data-wow-delay="0.2s" data-wow-duration="2s"> Touch with us</span></h1>
+				<h3>Lorem Ipsum is simply dummy text of the printing and typesetting industry</h3>
+			</div>
+			<div className="contact-section">
+			<div className="container">
+				<form>
+					<div className="col-md-6 form-line">
+			  			<div className="form-group">
+			  				<label className="exampleInputUsername">Your name</label>
+					    	<input type="text" className="form-control" id="" placeholder=" Enter Name"/>
+				  		</div>
+				  		<div className="form-group">
+					    	<label className="exampleInputEmail">Email Address</label>
+					    	<input type="email" className="form-control" id="exampleInputEmail" placeholder=" Enter Email id"/>
+					  	</div>	
+					  	<div className="form-group">
+					    	<label className="telephone">Mobile No.</label>
+					    	<input type="tel" className="form-control" id="telephone" placeholder=" Enter 10-digit mobile no."/>
+			  			</div>
+			  		</div>
+			  		<div className="col-md-6">
+			  			<div className="form-group">
+			  				<label className ="description"> Message</label>
+			  			 	<textarea  className="form-control" id="description" placeholder="Enter Your Message"></textarea>
+			  			</div>
+			  			<div>
 
-        <p className="dddd">
-          WE START WOKING ON LABASNI SINCE 2021
-          <code className="ddd"></code>.
-        </p>
-
-       
-
-        <div className="ddd">
-          <a href="#" className="ddd">
-            <h2>histroy &rarr;</h2>
-            <p>WE ARE IN THE E-COMMERCE SINCE 2020
-              AND LABASNI IS OUR THIRD PROJECT 
-            </p>
-          </a>
-
-         <div className="ddd">
-            <h2>OUR SERVICE &rarr;</h2>
-            <p>WE are presanting the high quallity 
-              and the offical product .<br></br>
-              we are the first 
-            </p>
-            </div>
-
-         <div className="ddd">
-            <h2>OUR BRANDS &rarr;</h2>
-            <p>NIKE ,UNDERARMOUR,JORDAN<br></br>PUMA,NBA,MLBS<br></br> FOOT JERESEY FOOT
-            </p>
-            </div>
-
-          
-        </div>
-      </main>
+			  				<button type="button" className="btn btn-default submit"><i className="fa fa-paper-plane" aria-hidden="true"></i>  Send Message</button>
+			  			</div>
+			  			
+					</div>
+				</form>
+			</div>
+      </div>
+		</section>
   </div>
 }
--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app' 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "../styles/Contact.css"
 import Layout from '../components/layout'
 
 function MyApp({ Component, pageProps }:any) {
